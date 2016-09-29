ba6f4d9decf7295bb2f3872c882ba0246dfc2722
yuiofthesun <mofirre@gmail.com>
2016-09-29 14:04:05 +0200

    Added the server. Also rearanged some files during testing

---
+++ server/server.go
@@ -0,0 +1,16 @@
+
+
+package main
+
+
+import "net/http"
+
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello! \n"))
+} 
